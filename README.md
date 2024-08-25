718de043c0556f23fa1d6286de466ed00c92a048
Gusti Bagus Suandana <gusti@vinceredigital.com>
2024-08-25 13:11:56 +0800

    readme privacy terms

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NBK Chord
+
+NBK Chord is a web application that provides a collection of songs from the Salvation Army songbook. This app features the ability to transpose chords, making it easier for musicians to play in their preferred key.
+
+## Features
+
+- **Song Collection**: Access a comprehensive list of songs from the Salvation Army songbook.
+- **Transpose Chords**: Easily change the key of any song to fit your vocal range or instrument tuning.
+- **Responsive Design**: Enjoy a seamless experience on both desktop and mobile devices.
+
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -12,37 +12,37 @@ import appConfig from "@/app.config";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: appConfig.title,
-  description: appConfig.description,
+    title: appConfig.title,
+    description: appConfig.description,
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <ProviderTheme>
-        <body
-          className={
-            "relative min-h-screen font-mono flex flex-col justify-between"
-          }
-        >
-          <div className={"flex-1"}>
-            <Header />
-            <main
-              className={
-                "md:mb-12 mb-8 min-w-full prose md:prose-lg dark:prose-invert"
-              }
-            >
-              {children}
-            </main>
-          </div>
-          <Footer />
-          <BackToTop />
-        </body>
-      </ProviderTheme>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <ProviderTheme>
+                <body
+                    className={
+                        "relative min-h-screen font-inter flex flex-col justify-between"
+                    }
+                >
+                    <div className={"flex-1"}>
+                        <Header />
+                        <main
+                            className={
+                                "md:mb-12 my-8 min-w-full prose md:prose-lg dark:prose-invert"
+                            }
+                        >
+                            {children}
+                        </main>
+                    </div>
+                    <Footer />
+                    <BackToTop />
+                </body>
+            </ProviderTheme>
+        </html>
+    );
 }
